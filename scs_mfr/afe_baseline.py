69f94ce6e850b591b1bcccc44dce86419f9e8695
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-03-01 12:11:42 +0000

    Added AFEBaseline class.

---
+++ scs_mfr/afe_baseline.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+"""
+Created on 1 Mar 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+Creates AFECalib document.
+
+command line example:
+./scs_mfr/afe_calib.py -v -s 15-000064
+"""
+
+import datetime
+import sys
+
+from scs_core.data.json import JSONify
+
+from scs_dfe.gas.afe_baseline import AFEBaseline
+from scs_dfe.gas.sensor_baseline import SensorBaseline
+
+from scs_host.sys.host import Host
+
+from scs_mfr.cmd.cmd_afe_baseline import CmdAFEBaseline
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # cmd...
+
+    cmd = CmdAFEBaseline()
+
+    if cmd.verbose:
+        print(cmd, file=sys.stderr)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # run...
+
+    now = datetime.datetime.now()
+
+    baseline = AFEBaseline.load_from_host(Host)
+
+    if cmd.set():
+        for i, offset in cmd.offsets.items():
+            if offset is not None:
+                baseline.set_sensor_baseline(i, SensorBaseline(now.date(), offset))
+
+        baseline.save(Host)
+
+    print(JSONify.dumps(baseline))
