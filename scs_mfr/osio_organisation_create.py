9c0d0c8ae17d6a0281ca7d58a1a4a109bb937b63
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-03-08 13:46:13 +0000

    Added OSIO Organisation class.

--- scs_mfr/osio_organisation_create.py
+++ scs_mfr/osio_organisation_create.py
@@ -8,13 +8,14 @@ Created on 8 Mar 2017
 Requires APIAuth document.
 
 command line examples:
-./scs_mfr/osio_organisation_create.py -v
+./scs_mfr/osio_organisation_create.py -v -o test-org-1 -n "Test Org 1" -w www.southcoastscience.com \
+-d "a test organisation" -e test1@southcoastscience.com
 """
 
 import sys
 
+from scs_core.data.json import JSONify
 from scs_core.osio.client.api_auth import APIAuth
-
 from scs_core.osio.data.organisation import Organisation
 from scs_core.osio.manager.organisation_manager import OrganisationManager
 
@@ -68,5 +69,6 @@ if __name__ == '__main__':
     org = Organisation(cmd.org_id, cmd.name, cmd.website, cmd.description, cmd.email)
 
     # create device...
-    response = manager.create(org)
-    print(response)
+    manager.create(org)
+
+    print(JSONify.dumps(org))
