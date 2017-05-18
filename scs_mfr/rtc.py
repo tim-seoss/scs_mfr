4d8a2580faba4b5fee32776bc06423c349c980ca
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-05-18 10:29:16 +0100

    Added rtc top-level script to scs_mfr.

--- scs_mfr/rtc.py
+++ scs_mfr/rtc.py
@@ -7,21 +7,64 @@ Created on 18 May 2017
 """
 
 import sys
+import tzlocal
 
 from scs_core.data.json import JSONify
-from scs_core.gas.pt1000_calib import Pt1000Calib
-from scs_core.sys.exception_report import ExceptionReport
+from scs_core.data.localized_datetime import LocalizedDatetime
+from scs_core.data.rtc_datetime import RTCDatetime
 
-from scs_dfe.climate.sht_conf import SHTConf
-from scs_dfe.gas.afe import AFE
-from scs_dfe.gas.pt1000 import Pt1000
+from scs_dfe.time.ds1338 import DS1338
 
 from scs_host.bus.i2c import I2C
 from scs_host.sys.host import Host
 
-from scs_mfr.cmd.cmd_pt1000_calib import CmdPt1000Calib
+from scs_mfr.cmd.cmd_rtc import CmdRTC
 
 
 # --------------------------------------------------------------------------------------------------------------------
 
-# TODO: implement RTC init / set functions
+if __name__ == '__main__':
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # cmd...
+
+    cmd = CmdRTC()
+
+    if cmd.verbose:
+        print(cmd, file=sys.stderr)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # resources...
+
+    now = LocalizedDatetime.now()
+
+    if cmd.verbose:
+        print("system time: %s" % now, file=sys.stderr)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # run...
+
+    try:
+        I2C.open(Host.I2C_SENSORS)
+
+        if cmd.initialise:
+            DS1338.init()
+
+        if cmd.set:
+            rtc_datetime = RTCDatetime.construct_from_localized_datetime(now)
+            DS1338.set_time(rtc_datetime)
+
+        # get current...
+        rtc_datetime = DS1338.get_time()
+        localized_datetime = rtc_datetime.as_localized_datetime(tzlocal.get_localzone())
+
+        if cmd.verbose:
+            print("rtc datetime: %s" % localized_datetime, file=sys.stderr)
+            sys.stderr.flush()
+
+        print(JSONify.dumps(localized_datetime))
+
+    finally:
+        I2C.close()
