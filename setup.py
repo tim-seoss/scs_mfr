fa8fc74769e1cfa6c9b7b51de21200f130817079
Gemma Hentsch <gemma@halfapenguin.com>
2018-01-16 10:22:29 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,29 @@
-# Package and distribution management.
+from setuptools import setup, find_packages
 
+
+with open('requirements.txt') as req_txt:
+    required = [line for line in req_txt.read().splitlines() if line]
+
+setup(
+    name='scs_mfr',
+    version='0.1.0',
+    description='High-level scripts and command-line applications for South Coast Science environmental monitor manufacturing, test and calibration.',
+    author='South Coast Science',
+    author_email='contact@southcoastscience.com',
+    url='https://github.com/south-coast-science/scs_mfr',
+    package_dir={'':'src'},
+    packages=find_packages('src'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: POSIX',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=required,
+    platforms=['any'],
+    python_requires=">=3.3",
+)
