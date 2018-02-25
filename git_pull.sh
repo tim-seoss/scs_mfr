292f024800900527d96952b7507449ca0484e243
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-02-25 12:43:19 +0000

    Added git_pull.sh

---
+++ git_pull.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# An example git pull script, normally copied to ~/SCS and incorporating all repos
+
+GIT_PATH=~/SCS/scs_mfr/
+echo ${GIT_PATH}
+git -C ${GIT_PATH} pull
+echo '-'
+
+GIT_PATH=~/SCS/scs_core/
+echo ${GIT_PATH}
+git -C ${GIT_PATH} pull
+echo '-'
+
+GIT_PATH=~/SCS/scs_dfe_eng/
+echo ${GIT_PATH}
+git -C ${GIT_PATH} pull
+echo '-'
+
+GIT_PATH=~/SCS/scs_host_posix/          # replace with the appropriate host package
+echo ${GIT_PATH}
+git -C ${GIT_PATH} pull
+echo '-'
+
+GIT_PATH=~/SCS/scs_osio/
+echo ${GIT_PATH}
+git -C ${GIT_PATH} pull
+echo '-'
