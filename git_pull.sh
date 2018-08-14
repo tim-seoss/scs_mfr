b8edc013e316f9dad73e8b1d44afe32cad07d896
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-08-14 09:42:03 +0100

    Updated git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -17,7 +17,7 @@ echo ${GIT_PATH}
 git -C ${GIT_PATH} pull
 echo '-'
 
-GIT_PATH=~/SCS/scs_host_posix/          # replace with the appropriate host package
+GIT_PATH=~/SCS/scs_host_rpi/          # replace with the appropriate host package
 echo ${GIT_PATH}
 git -C ${GIT_PATH} pull
 echo '-'
