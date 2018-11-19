8d6c00b03c8626ef846f0c6c98cd59c4d76d6b1a
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-11-19 20:22:29 +0000

    Added sample_timezone.py top-level script.

--- src/scs_mfr/timezone.py
+++ src/scs_mfr/timezone.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
 
     elif cmd.set():
         if not Timezone.is_valid(cmd.zone):
-            print("timezone: zone is not accepted: %s" % cmd.zone, file=sys.stderr)
+            print("timezone: unrecognised name: %s" % cmd.zone, file=sys.stderr)
             exit(1)
 
         if cmd.zone != conf.name:
