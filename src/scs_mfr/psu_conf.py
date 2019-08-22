0dffb4eabfaf07ce6d3ec35c4d69aac824386129
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-08-22 14:33:41 +0100

    Updated documentation.

--- src/scs_mfr/psu_conf.py
+++ src/scs_mfr/psu_conf.py
@@ -9,9 +9,10 @@ DESCRIPTION
 The psu_conf utility is used to specify whether a South Coast Science power supply (PSU) board is present
 and if so, which model is provided. Two models are currently available:
 
-* MobileV1
-* OsloV1
-* PrototypeV1
+* MobileV1 via PZHBt1
+* MobileV1 via PZHBt2
+* PrototypeV1 via serial port
+* OsloV1 via serial port
 
 Note that the scs_dev/status_sampler process must be restarted for changes to take effect.
 
