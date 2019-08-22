8f8aeed65aaaade391cd19e1988deed26aa102ad
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-08-22 11:52:09 +0100

    Added PZHBMCUt2f1 Pi Zero header microcontroller driver.

--- src/scs_mfr/test/eeprom_test.py
+++ src/scs_mfr/test/eeprom_test.py
@@ -12,7 +12,7 @@ import sys
 
 from scs_core.sys.eeprom_image import EEPROMImage
 
-from scs_dfe.interface.components.cat24c32 import CAT24C32
+from scs_dfe.interface.component.cat24c32 import CAT24C32
 
 from scs_host.bus.i2c import I2C
 from scs_host.sys.host import Host
