6f12e6c684d38a94617a9aadc1dc74f6dde57533
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-10-24 12:45:54 +0100

    Refactored NDIR classes.

--- src/scs_mfr/ndir_conf.py
+++ src/scs_mfr/ndir_conf.py
@@ -42,7 +42,7 @@ from scs_host.sys.host import Host
 
 from scs_mfr.cmd.cmd_ndir_conf import CmdNDIRConf
 
-from scs_ndir.gas.ndir_conf import NDIRConf
+from scs_ndir.gas.ndir.ndir_conf import NDIRConf
 
 
 # --------------------------------------------------------------------------------------------------------------------
