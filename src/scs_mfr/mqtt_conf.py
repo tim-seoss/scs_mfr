da80c92b25b8960b8e31930ebbcb278f08dd0607
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-01-28 16:50:18 +0000

    Removed queue length from MQTTConf

--- src/scs_mfr/mqtt_conf.py
+++ src/scs_mfr/mqtt_conf.py
@@ -12,10 +12,6 @@ The INHIBIT_PUBLISHING field determines whether the MQTT client will publish mes
 then the client will continue to accept messages and echo these to stdout if required, but will not publish. This
 setting is useful when the device is used in an offline mode - in this case data is typically written to a CSV file.
 
-The QUEUE_SIZE field determines how many messages can be buffered before publication. Where a device is being used in
-an environment with unreliable communications, this could potentially be a large number of messages - the
-default queue size is 21000, equivalent to just over 24 hours of data under a normal sampling regime.
-
 The REPORT_FILE parameter, if set, indicates where the latest queue length value should be stored.
 
 If DEBUG is set to 1 (true), then extra logging will be written to stderr by the MQTT client.
@@ -26,14 +22,13 @@ WARNING: if inhibit publishing is set to true, the MQTT client will still subscr
 receipts or responses.
 
 SYNOPSIS
-mqtt_conf.py { [-p INHIBIT_PUBLISHING] [-q QUEUE_SIZE] [-f REPORT_FILE]  [-l { 0 | 1 }] | -d } [-v]
+mqtt_conf.py { [-p INHIBIT_PUBLISHING] [-f REPORT_FILE]  [-l { 0 | 1 }] | -d } [-v]
 
 EXAMPLES
-./mqtt_conf.py -p 0 -q 21000 -f /tmp/southcoastscience/mqtt_queue_length.json -l 1
+./mqtt_conf.py -p 0 -f /tmp/southcoastscience/mqtt_queue_length.json -l 1
 
 DOCUMENT EXAMPLE
-{"inhibit-publishing": false, "queue-size": 21000, "report-file": "/tmp/southcoastscience/mqtt_queue_length.json",
-"debug": true}
+{"inhibit-publishing": false, "report-file": "/tmp/southcoastscience/mqtt_queue_report.json", "debug": true}
 
 FILES
 ~/SCS/conf/mqtt_conf.json
@@ -83,14 +78,10 @@ if __name__ == '__main__':
 
     if cmd.set():
         inhibit_publishing = cmd.inhibit_publishing if cmd.inhibit_publishing is not None else conf.inhibit_publishing
-        queue_size = cmd.queue_size if cmd.queue_size is not None else conf.queue_size
         report_file = cmd.report_file if cmd.report_file is not None else conf.report_file
         debug = cmd.debug if cmd.debug is not None else conf.debug
 
-        if queue_size is None:
-            queue_size = MQTTConf.DEFAULT_QUEUE_SIZE
-
-        conf = MQTTConf(inhibit_publishing, queue_size, report_file, debug)
+        conf = MQTTConf(inhibit_publishing, report_file, debug)
         conf.save(Host)
 
     elif cmd.delete and conf is not None:
