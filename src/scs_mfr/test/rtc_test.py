7f2e0e660a79cb481a2fa420f295ddb599e440ed
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-02-11 14:04:12 +0000

    Added timer.py top-level script

--- src/scs_mfr/test/rtc_test.py
+++ src/scs_mfr/test/rtc_test.py
@@ -8,7 +8,7 @@ import sys
 import time
 import tzlocal
 
-from scs_core.data.localized_datetime import LocalizedDatetime
+from scs_core.data.datetime import LocalizedDatetime
 from scs_core.data.rtc_datetime import RTCDatetime
 
 from scs_dfe.time.ds1338 import DS1338
