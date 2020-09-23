c22e4dbe43da8d939fc6e7d23b63f13ebae86f92
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-09-23 09:26:17 +0100

    Cleaning

--- src/scs_mfr/aws_group_setup.py
+++ src/scs_mfr/aws_group_setup.py
@@ -22,23 +22,26 @@ A conf file is placed in a default directory referencing the group name and when
 
 https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/greengrass.html
 """
-import sys
-from getpass import getpass
 import boto3
+from getpass import getpass
+import sys
+
 from botocore.exceptions import ClientError
 
-from scs_core.aws.greengrass.gg_exceptions import ProjectMissingError
-from scs_core.data.json import JSONify
-from scs_host.sys.host import Host
-from scs_core.aws.greengrass.aws_group_configurator import AWSGroupConfigurator
 from scs_core.aws.greengrass.aws_group import AWSGroup
+from scs_core.aws.greengrass.aws_group_configurator import AWSGroupConfigurator
+from scs_core.aws.greengrass.gg_exceptions import ProjectMissingError
+
+from scs_core.data.json import JSONify
+
+from scs_host.sys.host import Host
+
 from scs_mfr.cmd.cmd_aws_group_setup import CmdAWSGroupSetup
 
 
 # --------------------------------------------------------------------------------------------------------------------
 
 def create_aws_client():
-
     access_key_secret = ""
     access_key_id = input("Enter AWS Access Key ID or leave blank to use environment variables: ")
     if access_key_id:
@@ -53,8 +56,11 @@ def create_aws_client():
         )
     else:
         client = boto3.client('greengrass', region_name=aws_region)
+
     return client
 
+
+# --------------------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
 
@@ -94,22 +100,26 @@ if __name__ == '__main__':
                 exit()
         try:
             aws_configurator = AWSGroupConfigurator(cmd.aws_group_name, create_aws_client(), cmd.use_ml)
+
             aws_configurator.collect_information(Host)
             aws_configurator.define_aws_group_resources(Host)
             aws_configurator.define_aws_group_functions()
             aws_configurator.define_aws_group_subscriptions()
             aws_configurator.create_aws_group_definition()
             aws_configurator.save(Host)
+
         except ClientError as error:
             if error.response['Error']['Code'] == 'BadRequestException':
                 print("aws_group_setup: Invalid request.", file=sys.stderr)
             if error.response['Error']['Code'] == 'InternalServerErrorException':
                 print("aws_group_setup: AWS server error.", file=sys.stderr)
+
         except ProjectMissingError:
-                print("aws_group_setup: Project configuration not set.", file=sys.stderr)
+            print("aws_group_setup: Project configuration not set.", file=sys.stderr)
 
     if cmd.show_current:
         aws_group_info = AWSGroup(cmd.aws_group_name, create_aws_client())
+
         aws_group_info.get_group_info_from_name()
         aws_group_info.get_group_arns()
         aws_group_info.output_current_info()
