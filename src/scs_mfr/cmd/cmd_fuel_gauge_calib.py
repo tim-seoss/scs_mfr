f6d9098594a422e554470bc1cf872c3ed132c517
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-04-16 16:17:20 +0100

    Upgraded fuel_gauge_calib.py

--- src/scs_mfr/cmd/cmd_fuel_gauge_calib.py
+++ src/scs_mfr/cmd/cmd_fuel_gauge_calib.py
@@ -1,5 +1,5 @@
 """
-Created on 4 Apr 2020
+Created on 16 Apr 2021
 
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 """
@@ -16,33 +16,42 @@ class CmdFuelGaugeCalib(object):
         """
         Constructor
         """
-        self.__parser = optparse.OptionParser(usage="%prog { { -n | -l { D | F } | -s } | "
-                                                    "{ -c | -f | -p } [-i INTERVAL] } "
-                                                    "[-v]", version="%prog 1.0")
+        self.__parser = optparse.OptionParser(usage="%prog { { -c | -d | -l | -s | -r | -z { D | L } } | "
+                                                    "{ -f | -g | -p } [-i INTERVAL] } [-v]", version="%prog 1.0")
 
-        # single shot...
-        self.__parser.add_option("--initialise", "-n", action="store_true", dest="initialise", default=False,
-                                 help="initialise the fuel gauge configuration")
+        # configuration...
+        self.__parser.add_option("--gauge-conf", "-c", action="store_true", dest="gauge_conf", default=False,
+                                 help="report fuel gauge configuration")
 
-        self.__parser.add_option("--load", "-l", type="string", nargs=1, action="store", dest="load",
-                                 help="load fuel gauge parameters from Default or Filesystem")
+        # single-shot...
+        self.__parser.add_option("--dflt-learned", "-d", action="store_true", dest="default_learned", default=False,
+                                 help="report battery's default params")
 
-        self.__parser.add_option("--save", "-s", action="store_true", dest="save", default=False,
-                                 help="save the current fuel gauge parameters to filesystem")
+        self.__parser.add_option("--host-learned", "-l", action="store_true", dest="host_learned", default=False,
+                                 help="read params from host")
+
+        self.__parser.add_option("--save-learned", "-s", action="store_true", dest="save_learned", default=False,
+                                 help="save learned params to host")
+
+        self.__parser.add_option("--remove-learned", "-r", action="store_true", dest="remove_learned", default=False,
+                                 help="delete params from host")
+
+        self.__parser.add_option("--init", "-z", type="string", nargs=1, action="store", dest="init",
+                                 help="initialise gauge with configuration and Default or Learned params")
 
         # iterable...
-        self.__parser.add_option("--current", "-c", action="store_true", dest="current", default=False,
-                                 help="report the current fuel gauge parameters")
+        self.__parser.add_option("--gauge-learned", "-g", action="store_true", dest="gauge_learned", default=False,
+                                 help="read learned params from gauge")
 
         self.__parser.add_option("--fuel", "-f", action="store_true", dest="fuel", default=False,
-                                 help="sample the fuel gauge")
+                                 help="report fuel status")
 
-        self.__parser.add_option("--power", "-p", action="store_true", dest="power", default=False,
-                                 help="sample the PSU")
+        self.__parser.add_option("--psu", "-p", action="store_true", dest="psu", default=False,
+                                 help="report psu status")
 
-        # optional...
-        self.__parser.add_option("--interval", "-i", type="float", nargs=1, action="store", dest="interval",
-                                 help="sampling interval in seconds")
+        # output...
+        self.__parser.add_option("--interval", "-i", type="int", nargs=1, action="store", dest="interval",
+                                 help="sampling interval")
 
         self.__parser.add_option("--verbose", "-v", action="store_true", dest="verbose", default=False,
                                  help="report narrative to stderr")
@@ -55,31 +64,40 @@ class CmdFuelGaugeCalib(object):
     def is_valid(self):
         count = 0
 
-        if self.initialise:
+        if self.gauge_conf:
             count += 1
 
-        if self.load:
+        if self.default_learned:
             count += 1
 
-        if self.save:
+        if self.host_learned:
             count += 1
 
-        if self.current:
+        if self.save_learned:
+            count += 1
+
+        if self.remove_learned:
+            count += 1
+
+        if self.init is not None:
+            count += 1
+
+        if self.gauge_learned:
             count += 1
 
         if self.fuel:
             count += 1
 
-        if self.power:
+        if self.psu:
             count += 1
 
         if count != 1:
             return False
 
-        if self.load and self.load not in ('D', 'F'):
+        if self.init is not None and self.init != 'D' and self.init != 'L':
             return False
 
-        if (self.initialise or self.load or self.save) and self.interval:
+        if self.__opts.interval is not None and not self.gauge_learned and not self.fuel and not self.psu:
             return False
 
         return True
@@ -88,23 +106,38 @@ class CmdFuelGaugeCalib(object):
     # ----------------------------------------------------------------------------------------------------------------
 
     @property
-    def initialise(self):
-        return self.__opts.initialise
+    def gauge_conf(self):
+        return self.__opts.gauge_conf
 
 
     @property
-    def load(self):
-        return self.__opts.load
+    def default_learned(self):
+        return self.__opts.default_learned
 
 
     @property
-    def save(self):
-        return self.__opts.save
+    def host_learned(self):
+        return self.__opts.host_learned
 
 
     @property
-    def current(self):
-        return self.__opts.current
+    def save_learned(self):
+        return self.__opts.save_learned
+
+
+    @property
+    def remove_learned(self):
+        return self.__opts.remove_learned
+
+
+    @property
+    def init(self):
+        return self.__opts.init
+
+
+    @property
+    def gauge_learned(self):
+        return self.__opts.gauge_learned
 
 
     @property
@@ -113,8 +146,8 @@ class CmdFuelGaugeCalib(object):
 
 
     @property
-    def power(self):
-        return self.__opts.power
+    def psu(self):
+        return self.__opts.psu
 
 
     @property
@@ -134,7 +167,7 @@ class CmdFuelGaugeCalib(object):
 
 
     def __str__(self, *args, **kwargs):
-        return "CmdFuelGaugeCalib:{initialise:%s, load:%s, save:%s, " \
-               "current:%s, fuel:%s, power:%s, interval:%s, verbose:%s}" % \
-               (self.initialise, self.load, self.save,
-                self.current, self.fuel, self.power, self.interval, self.verbose)
+        return "CmdFuelGaugeCalib:{gauge_conf:%s, default_learned:%s, host_learned:%s, save_learned:%s, " \
+               "remove_learned:%s, init:%s, gauge_learned:%s, fuel:%s, psu:%s, interval:%s, verbose:%s}" % \
+               (self.gauge_conf, self.default_learned, self.host_learned, self.save_learned, self.remove_learned,
+                self.init, self.gauge_learned, self.fuel, self.psu, self.__opts.interval, self.verbose)
--- src/scs_mfr/fuel_gauge_calib.py
+++ src/scs_mfr/fuel_gauge_calib.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Created on 4 Apr 2020
+Created on 16 Apr 2021
 
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 
@@ -14,44 +14,46 @@ battery pack model incorporates a set of parameter values gained through this pr
 referred to as the default parameters.
 
 When a new fuel gauge is put into use, it should be initialised with these values using the fuel_gauge_calib utility
---initialise flag - this sets both the parameters and the fuel gauge configuration. If a max17055_params.json
-configuration file has been stored for this system, then it is used to initialise the parameters. Otherwise, the
-default parameters for the configured battery pack are used.
+--initialise flag - this sets both the parameters and the fuel gauge configuration.
 
 SYNOPSIS
-fuel_gauge_calib.py { { -n | -l { D | F } | -s } | { -c | -f | -p } [-i INTERVAL] } [-v]
+fuel_gauge_calib.py { { -c | -d | -l | -s | -r | -z { D | L } } | { -f | -g | -p } [-i INTERVAL] } [-v]
 
 EXAMPLES
-./fuel_gauge_calib.py -cv
+./batt_conf.py -i5 -f
+
+DOCUMENT EXAMPLE - CONFIGURATION
+{"des-cap": 3000, "sense-res": 0.01, "chrg-term": 10, "empty-v-target": 3.3, "recovery-v": 3.8, "chrg-v": 0,
+"batt-type": 0}
+
+DOCUMENT EXAMPLE - LEARNED PARAMS
+{"calibrated-on": "2021-01-03T09:25:52Z", "r-comp-0": 255, "temp-co": 9278, "full-cap-rep": 3000, "full-cap-nom": 3000,
+"cycles": 150}
+
+DOCUMENT EXAMPLE - FUEL
+{"in": false, "chrg": {"%": 1.0, "mah": 0}, "tte": null, "ttf": null, "v": 3.8, "curr": -486, "g-tmp": 25.5, "cap": 0,
+"cyc": 2.0}
+
+DOCUMENT EXAMPLE - PSU
+{"src": "Cv1", "standby": false, "in": false, "pwr-in": 0.1, "chgr": "TFFT",
+"batt": {"chg": 1, "tte": null, "ttf": null}, "prot-batt": 3.8}
 
 FILES
 ~/SCS/conf/max17055_params.json
 
-DOCUMENT EXAMPLE - PARAMETERS
-{"calibrated-on": "2021-01-02T09:34:48Z",
-"r-comp-0": 201, "temp-co": 9278, "full-cap-rep": 1790, "full-cap-nom": 4896, "cycles": 210}
-
-DOCUMENT EXAMPLE - FUEL
-{"in": true, "chrg": {"%": 99.0, "mah": 1729}, "tte": null, "ttf": "00-02:55:24", "v": 4.1, "curr": 182,
-"g-tmp": 30.7, "cap": 1729, "cyc": 2.9}
-
-DOCUMENT EXAMPLE - PSU
-{"src": "Cv1", "standby": false, "in": true, "pwr-in": 11.6, "chgr": "TFTF",
-"batt": {"chg": 99, "tte": null, "ttf": "00-02:54:56"}, "prot-batt": 4.1}
-
 SEE ALSO
+scs_dev/psu
 scs_dev/psu_monitor
-scs_mfr/psu_conf
 """
 
 import sys
 
 from scs_core.data.json import JSONify
 from scs_core.sync.interval_timer import IntervalTimer
+from scs_core.sys.logging import Logging
 
 from scs_dfe.interface.interface_conf import InterfaceConf
 
-from scs_host.bus.i2c import I2C
 from scs_host.sys.host import Host
 
 from scs_mfr.cmd.cmd_fuel_gauge_calib import CmdFuelGaugeCalib
@@ -64,8 +66,6 @@ from scs_psu.psu.psu_conf import PSUConf
 
 if __name__ == '__main__':
 
-    params = None
-
     # ----------------------------------------------------------------------------------------------------------------
     # cmd...
 
@@ -75,58 +75,82 @@ if __name__ == '__main__':
         cmd.print_help(sys.stderr)
         exit(2)
 
-    if cmd.verbose:
-        print("fuel_gauge_calib: %s" % cmd, file=sys.stderr)
-        sys.stderr.flush()
+    # logging...
+    Logging.config('fuel_gauge_calib', verbose=cmd.verbose)
+    logger = Logging.getLogger()
+
+    logger.info(cmd)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # resources...
+
+    # InterfaceConf...
+    interface_conf = InterfaceConf.load(Host)
+
+    if interface_conf is None:
+        logger.error('InterfaceConf not available.')
+        exit(1)
+
+    interface_model = interface_conf.model
+
+    # PSUConf...
+    psu_conf = PSUConf.load(Host)
+
+    if psu_conf is None:
+        logger.error('PSUConf not available.')
+        exit(1)
+
+    logger.info(psu_conf)
+
+    psu = psu_conf.psu(Host, interface_model)
+    batt_pack = psu.batt_pack
+
+    if batt_pack is None:
+        logger.error('No battery pack available.')
+        exit(1)
+
+    logger.info(batt_pack)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # run...
 
     try:
-        I2C.Sensors.open()
-
-        # ------------------------------------------------------------------------------------------------------------
-        # resources...
-
-        # Interface...
-        interface_conf = InterfaceConf.load(Host)
-
-        # PSU...
-        psu_conf = PSUConf.load(Host)
-        psu = psu_conf.psu(Host, interface_conf.model)
-
-        if cmd.verbose:
-            print("fuel_gauge_calib: %s" % psu, file=sys.stderr)
-
-        batt_pack = psu.batt_pack
-
-        if batt_pack is None:
-            print("fuel_gauge_calib: PSU has no battery pack.", file=sys.stderr)
-            exit(1)
-
-
-        # ------------------------------------------------------------------------------------------------------------
-        # run...
-
-        # no auto-initialisation - we want to see the Max17055 native values
-
         # single shot...
-        if cmd.initialise:
-            params = batt_pack.initialise(Host, force_config=True)
-            params.save(Host)
+        if cmd.gauge_conf:
+            gauge_conf = batt_pack.gauge_conf()
+            print(JSONify.dumps(gauge_conf))
+            exit(0)
 
-        elif cmd.load == 'D':
+        if cmd.default_learned:
             params = batt_pack.default_params()
-            batt_pack.write_params(params)
-            params.save(Host)
+            print(JSONify.dumps(params))
+            exit(0)
 
-        elif cmd.load == 'F':
+        if cmd.host_learned:
             params = Max17055Params.load(Host)
-            batt_pack.write_params(params)
-            params.save(Host)
+            if params:
+                print(JSONify.dumps(params))
+            exit(0)
 
-        elif cmd.save:
+        if cmd.save_learned:
             params = batt_pack.read_learned_params()
             params.save(Host)
+            print(JSONify.dumps(params))
+            exit(0)
 
-        if cmd.initialise or cmd.load or cmd.save:
+        if cmd.remove_learned:
+            Max17055Params.delete(Host)
+            exit(0)
+
+        if cmd.init is not None:
+            if cmd.init == 'D':
+                Max17055Params.delete(Host)
+                params = batt_pack.default_params()
+                params.save(Host)
+
+            params = batt_pack.initialise(Host, force_config=True)
             print(JSONify.dumps(params))
             exit(0)
 
@@ -134,27 +158,20 @@ if __name__ == '__main__':
         timer = IntervalTimer(cmd.interval)
 
         while timer.true():
-            if cmd.current:
+            if cmd.gauge_learned:
                 params = batt_pack.read_learned_params()
                 print(JSONify.dumps(params))
 
-            elif cmd.fuel:
-                datum = batt_pack.sample()
-                print(JSONify.dumps(datum))
+            if cmd.fuel:
+                fuel = batt_pack.sample()
+                print(JSONify.dumps(fuel))
 
-            elif cmd.power:
+            if cmd.psu:
                 datum = psu.status()
                 print(JSONify.dumps(datum))
 
             if not cmd.interval:
                 break
 
-
-    # ----------------------------------------------------------------------------------------------------------------
-    # end...
-
     except KeyboardInterrupt:
         print(file=sys.stderr)
-
-    finally:
-        I2C.Sensors.close()
