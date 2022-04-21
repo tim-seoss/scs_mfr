80400fd2ff634beb1d1e9b1fb7e1a72fe55b419b
Bruno Beloff <bruno.beloff@southcoastscience.com>
2022-04-21 11:27:35 +0100

    Refactored Cognito login managers

--- src/scs_mfr/cognito_device_identity.py
+++ src/scs_mfr/cognito_device_identity.py
@@ -34,7 +34,7 @@ import sys
 
 from scs_core.aws.security.cognito_device_finder import CognitoDeviceFinder
 from scs_core.aws.security.cognito_login_manager import CognitoDeviceLoginManager
-from scs_core.aws.security.cognito_device import CognitoDeviceCredentials, CognitoDeviceIdentity
+from scs_core.aws.security.cognito_device import CognitoDeviceCredentials
 
 from scs_core.data.json import JSONify
 
@@ -52,12 +52,7 @@ from scs_mfr.cmd.cmd_cognito_device_identity import CmdCognitoDeviceIdentity
 
 if __name__ == '__main__':
 
-    logger = None
-    gatekeeper = None
-    credentials = None
     auth = None
-    finder = None
-    report = None
 
     try:
         # ------------------------------------------------------------------------------------------------------------
@@ -104,25 +99,24 @@ if __name__ == '__main__':
 
         logger.info(auth)
 
-        exit(0)
 
         # ------------------------------------------------------------------------------------------------------------
         # resources...
 
-        # finder = CognitoDeviceFinder(requests)
+        finder = CognitoDeviceFinder(requests)
 
 
         # ------------------------------------------------------------------------------------------------------------
         # run...
 
-        # report = finder.find_by_tag(auth.id_token, credentials.tag)
+        report = finder.find_by_tag(auth.id_token, credentials.tag)
 
 
     # ----------------------------------------------------------------------------------------------------------------
     # end...
 
         if credentials is not None:
-            print(JSONify.dumps(credentials, indent=cmd.indent))
+            print(JSONify.dumps(report, indent=cmd.indent))
 
     except KeyboardInterrupt:
         print(file=sys.stderr)
