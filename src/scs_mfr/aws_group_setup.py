2dbe4afe008f892db382c35fea4ed27d3242a4b5
Bruno Beloff <bruno.beloff@southcoastscience.com>
2022-11-23 10:47:57 +0000

    Improved exception reporting

--- src/scs_mfr/aws_group_setup.py
+++ src/scs_mfr/aws_group_setup.py
@@ -188,4 +188,4 @@ if __name__ == '__main__':
         logger.error("credentials error.")
 
     except EndpointConnectionError as ex:
-        logger.error(ex)
+        logger.error("%s: %s" % (ex.__class__.__name__, ex))
